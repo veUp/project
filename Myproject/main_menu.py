aeeb18bc6d20cd8cd531217bb33c809feeb9a2b5
upY <troyanishka@gmail.com>
2022-08-08 19:53:16 +0300

    Добавил историю

--- Myproject/main_menu.py
+++ Myproject/main_menu.py
@@ -19,8 +19,8 @@ class Window:
         self.root.protocol("WM_DELETE_WINDOW",self.exit)
         self.focus()
     def exit(self):
+        self.flg_exit = True
         self.root.destroy()
-        self.flg_exit=True
 
     def add_mass(self):
         start = add_func.Window_add(self.root)
@@ -33,10 +33,13 @@ class Window:
         start = account.Mass_user(self.login)
         a = start.show_history()
         if start is not False:
-            for x in a:
-                print(f'Вес - {x[0]}, дата:{x[1]}/{x[2]}/{x[3]}')
+            ab = CrollBarWindow(self.root,a)
+            self.root.withdraw()
+            flg = ab.run()
+            if flg:
+                self.root.deiconify()
         else:
-            print('Пусто')
+            mb.showwarning('Ошибка','История пуста!')
 
     def run(self,login=None):
         self.login = login
@@ -49,3 +52,32 @@ class Window:
         self.root.grab_set()
         self.root.focus_set()
         self.root.wait_window()
+
+class CrollBarWindow:
+    def __init__(self,parent,data):
+        self.root = Toplevel(parent)
+        self.root.iconbitmap('ico.ico')
+        self.root.configure(bg='#f3f76a')
+        self.flg_exit = False
+        self.data = data
+
+    def drow_widgest(self):
+        Label(self.root,text='История изменения веса',bg='#f3f76a',font=16,justify=CENTER).pack(pady=10)
+        for x in self.data:
+            Label(self.root,text=f'Вес - {x[0]}, дата:{x[1]}/{x[2]}/{x[3]}',bg='#fcca72',justify=LEFT).pack(pady=5,padx=10)
+        Button(self.root,text='Выход',bg='#fcca72',command=self.exit,justify=LEFT).pack(padx=10)
+        self.focus()
+    def run(self):
+        self.drow_widgest()
+        if self.flg_exit == True:
+            return True
+        self.root.mainloop()
+
+    def focus(self):
+        self.root.grab_set()
+        self.root.focus_set()
+        self.root.wait_window()
+
+    def exit(self):
+        self.flg_exit = True
+        self.root.destroy()
